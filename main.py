15f16ff533b8ea2678cb880481cf66eace36ab4f
Foo Zhen Min <foozhenmin@gmail.com>
2024-06-11 17:03:54 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,65 @@
+# This is a comment. 
+
+#####################################################################
+# Comments are ignored by Python and are for reading purposes only. #
+# Comments in this file serve as a guide to help you not get lost.  #
+# They help you structure your code into sections so that it is     #
+# easier to navigate and find what you are looking for.             #
+#####################################################################
+
+# Import pygame & other modules here
+
+
+
+# Game constants here 
+
+
+
+
+
+
+# Create your display window and text here
+
+
+
+
+
+# Create your player image here
+
+
+
+
+
+
+# Create your enemy image here    
+
+
+
+
+
+
+# Create your function to move the enemies here
+
+
+
+
+
+
+
+# Create your function for collision here
+
+
+
+
+
+
+# Create your draw function here 
+
+
+
+
+
+# This calls the main() function when the code runs 
+
+
+
